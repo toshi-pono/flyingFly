a96b046d1b4fb261bace25695f5d0becc07dd0fa
toshi/pono000 <toshi.pono.h@gmail.com>
2020-08-13 15:55:03 +0900

    動くように

--- js/game.js
+++ js/game.js
@@ -1,27 +1,67 @@
-import { Fly, Tempura } from "Game.js";
+import { Fly, Tempura } from "./enemy.js";
 
 class Game {
-  constructor(screenWidth, screenHeight, textureMap) {
+  constructor(screenWidth, screenHeight, filePos) {
     this.level = 1; // ステージ状態のレベル
     this.animationCounter = 0; // ゲーム内部カウンター
 
+    this.filePos = filePos;
     // ゲームのコンテナ作成
     this.container = new PIXI.Container();
     this.container.x = 0;
     this.container.y = 0;
 
-    let background = new PIXI.Graphics()
-      .beginFill(0xffff00)
-      .drawRect(0, 0, screenWidth, screenHeight)
-      .endFill();
-    this.container.addChild(background);
+    this.container.addChild(
+      new PIXI.Graphics()
+        .beginFill(0xffff00)
+        .drawRect(0, 0, screenWidth, screenHeight)
+        .endFill()
+    );
 
     // ゲームのキャラクター描画
+    let generationCount = 10;
+    this.flys = new Array();
+    this.tempuras = new Array();
+
+    for (let i = 0; i < generationCount; i++) {
+      this.generateFly();
+    }
+    for (let i = 0; i < generationCount; i++) {
+      this.generateTempura();
+    }
   }
-  get view() {
+  get pixi() {
+    // pixi描画のオブジェクトを返す
     return this.container;
   }
-  animate() {}
+  generateFly() {
+    this.flys.push(
+      // ハエオブジェクトを生成
+      new Fly(PIXI.loader.resources[this.filePos + "buta.png"].texture)
+    );
+    this.container.addChild(this.flys[this.flys.length - 1].pixi);
+  }
+  generateTempura() {
+    this.tempuras.push(
+      // 天ぷらオブジェクトを生成
+      new Tempura(PIXI.loader.resources[this.filePos + "buta.png"].texture)
+    );
+    this.container.addChild(this.tempuras[this.tempuras.length - 1].pixi);
+  }
+  checkHit() {
+    // enemyが捕まったかを判定
+  }
+  animate() {
+    // ハエの位置更新
+    for (let i = 0; i < this.flys.length; i++) {
+      this.flys[i].update(0);
+    }
+    // 天ぷらの位置更新
+    for (let i = 0; i < this.tempuras.length; i++) {
+      this.tempuras[i].update(0);
+    }
+    window.requestAnimationFrame(this.animate.bind(this));
+  }
 }
 
 export { Game };
